638c1f137d587073ad7ef6063ae60108aa73b5d5
lucaslegoff3 <lucas.le-goff@mmibordeaux.com>
2025-10-10 10:23:11 +0200

    text copy update
    ty Alvina !

--- Assets/Script/CopyButtonScript.cs
+++ Assets/Script/CopyButtonScript.cs
@@ -4,10 +4,11 @@ using TMPro;
 public class CopyButtonScript : MonoBehaviour
 {
     public TMP_Text sourceText; // si tu utilises TextMeshPro
+    public string textToCopy;
 
     public void CopyText()
     {
-        ClipboardManager.clipboardText = sourceText.text;
+        ClipboardManager.clipboardText = textToCopy;
         Debug.Log("Texte copié : " + ClipboardManager.clipboardText);
     }
 }
