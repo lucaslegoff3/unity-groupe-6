eb58d76de880c95a9b83a90499e132dde7a75286
LineCab <line.cabaret@mmibordeaux.com>
2025-10-10 12:29:09 +0200

    Scale hover

--- Assets/Script/ClickableObject.cs
+++ Assets/Script/ClickableObject.cs
@@ -14,11 +14,44 @@ public class ClickableObject : MonoBehaviour
     [Header("État")]
     [SerializeField] private bool isClicked = false;
 
+    [Header("Effet de survol")]
+    [SerializeField] private float hoverScale = 1.1f;
+    [SerializeField] private float scaleSpeed = 8f;
+
+    private Vector3 originalScale;
+    private bool isHovered = false;
+
     public string ID => id;
     public string ObjectName => objectName;
     public string Description => description;
     public Sprite InventorySprite => inventorySprite;
     public bool IsClicked => isClicked;
+
+    private void Awake()
+    {
+        originalScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        Vector3 targetScale = isHovered ? originalScale * hoverScale : originalScale;
+        transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * scaleSpeed);
+    }
+
+    private void OnMouseEnter()
+    {
+        isHovered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        isHovered = false;
+    }
+
+    private void OnMouseDown()
+    {
+        OnClick();
+    }
 
     public virtual void OnClick()
     {
